dc42eddfe88bf6ea6fc70870194c0046e590f24d
Sebastian Dittmann <sebastian.dittmann@lego.com>
2025-01-09 09:06:11 +0100

    change profile picture

--- components/cv-header/cv-header.tsx
+++ components/cv-header/cv-header.tsx
@@ -4,7 +4,7 @@ export default function CvHeader(){
     return (
      <div className="flex items-center justify-center mt-52">
             <Image
-                src="https://media.licdn.com/dms/image/v2/C5603AQGHLipJrLsz6A/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1517238367103?e=1731542400&v=beta&t=38CHhDqBMyBopjdYuUIvToptU22ugaKNYvks8RpRpo4"
+                src="/images/profile.jpeg"
                 alt="Sebastian Dittmann"
                 width="200"
                 height="200"
