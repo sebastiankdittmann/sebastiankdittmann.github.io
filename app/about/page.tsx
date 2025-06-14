54f381e23a9abdb1d65ea77be3df75f2958a3780
Sebastian Dittmann <sebastian.dittmann@lego.com>
2025-06-14 19:05:08 +0200

    update landing page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,21 +1,22 @@
 'use client';
 
 import  SidebarHomepage from "@/components/sidebarHomepage/SidebarHomepage";
+import { Box } from "@chakra-ui/react";
 
 export default function AboutPage() {
     return (
         <SidebarHomepage>
-            <div className='flex flex-col items-center'>
-                <div className='max-w-screen-md w-full mx-auto'>
-                    <div className="flex items-center justify-center mt-52">
+            <Box className='flex flex-col items-center'>
+                <Box className='max-w-screen-md w-full mx-auto'>
+                    <Box className="flex items-center justify-center mt-20">
                         <h1 className="text-2xl font-bold">About</h1>
-                    </div>
-                    <div className='mt-10'>
+                    </Box>
+                    <Box className='mt-10'>
                         <p className="text-center">I am a passionate software engineer from Denmark. I love experimenting with new
                         concepts and technologies, and this is my space to experiment and learn.</p>
-                    </div>
-                </div>
-            </div>
+                    </Box>
+                </Box>
+            </Box>
         </SidebarHomepage>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import CvHeader from "@/app/components/cv-header/cv-header";
-import Experiences from "@/app/components/experiences/experiences";
 import SidebarHomepage from "@/components/sidebarHomepage/SidebarHomepage";
+import TextLink from "@/components/textLink/TextLink";
+import { Box } from "@chakra-ui/react";
 import { StrictMode } from "react";
 
 export default function Home() {
@@ -10,12 +10,25 @@ export default function Home() {
         <div>
             <StrictMode>
                 <SidebarHomepage>
-                    <div className='flex flex-col items-center'>
-                        <div className='max-w-screen-md w-full mx-auto'>
-                            <CvHeader />
-                            <Experiences />
-                        </div>
-                    </div>
+                    <Box className='flex flex-col items-center'>
+                        <Box className='max-w-screen-md w-full mx-auto'>
+                            <Box className="flex items-center justify-center  mt-20">
+                                <h1 className="text-2xl font-bold">Welcome!</h1>
+                            </Box>
+                            <Box className='mt-10'>
+                                <p className="text-center">
+                                    Welcome to my blog. Here I will share my experiences and learnings from my work as a
+                                    software engineer. Maybe you will find some of it useful. Enjoy!
+                                </p>
+                                <ul className="text-center mt-4">
+                                    <TextLink
+                                        href={`/blog/apple-script`}
+                                        header="14.06.2025"
+                                        text="Automating environment setup with AppleScript" />
+                                </ul>
+                            </Box>
+                        </Box>
+                    </Box>
                 </SidebarHomepage>
             </StrictMode>
         </div>
